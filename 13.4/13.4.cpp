9e7064e4ee207ef8aa82bb7b027454890e71ce74
alex <a>
2024-03-14 16:09:20 +0400

    Добавьте файлы проекта.

---
+++ 13.4/13.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    std::cout << "first num = ";
+    std::cin >> a;
+    std::cout << std::endl;
+
+    std::cout << "second num = ";
+    std::cin >> b;
+    std::cout << std::endl;
+
+    int mul = sum(a, b);
+    
+    std::cout << "sum = " << mul;
+
+    return 0;
+}
